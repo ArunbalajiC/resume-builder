89191096223bfbf932ee02f2d345e69e3d1ae48c
ArunbalajiC <67551762+ArunbalajiC@users.noreply.github.com>
2024-09-04 22:26:21 +0530

    Update resm.py

--- resm.py
+++ resm.py
@@ -4,6 +4,7 @@ import PyPDF2
 from duckduckgo_search import DDGS
 from docx2pdf import convert
 import re
+import comtypes.client
 
 # Function to extract text from PDF
 def extract_text_from_pdf(pdf_file):
